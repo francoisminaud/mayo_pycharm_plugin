17245ed8cdbeb2625537acab5a9c4ac4de40f880
GitHub Action <action@github.com>
2021-02-18 10:42:37 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/francoisminaud/mayopycharmplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.francoisminaud.mayopycharmplugin.services
+
+import com.github.francoisminaud.mayopycharmplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/francoisminaud/mayopycharmplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/francoisminaud/mayopycharmplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.francoisminaud.mayopycharmplugin.services
 
+import com.github.francoisminaud.mayopycharmplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
